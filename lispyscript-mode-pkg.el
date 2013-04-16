58e7c8b1b410789448caa91dded4c5fe9229aac4
Kris Jenkins <krisajenkins@gmail.com>
2013-04-16 11:27:02 +0100

    Initial commit.

---
+++ lispyscript-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "lispyscript-mode" "0.1" "Major mode for LispyScript code" (quote nil))
---
+++ lispyscript-mode.el
@@ -0,0 +1,31 @@
+(require 'lisp-mode)
+(require 'font-lock)
+
+;;;###autoload
+(defvar lispyscript-font-lock-defaults
+  `((,(rx "(" (group (or "macro")) (one-or-more whitespace) (group (one-or-more word)))
+  	 (1 font-lock-keyword-face)
+  	 (2 font-lock-function-name-face))
+  	(,(rx "(" (group (or "var" "set")) (one-or-more whitespace) (group (one-or-more word)))
+  	 (1 font-lock-keyword-face)
+  	 (2 font-lock-variable-name-face))
+  	(,(rx "(" (group (or "if" "function" "try" "catch" "require" "eval" "include")))
+  	 (1 font-lock-keyword-face))
+  	(,(rx "(" (group (or "each" "javascript" "str" "object" "template" "sequence")))
+  	 (1 font-lock-keyword-face))))
+
+;;;###autoload
+(define-derived-mode lispyscript-mode lisp-mode "LispyScript"
+  "Major mode for LispyScript"
+  (modify-syntax-entry ?_ "w" lispyscript-mode-syntax-table)
+  (modify-syntax-entry ?~ "w" lispyscript-mode-syntax-table)
+  (modify-syntax-entry ?- "w" lispyscript-mode-syntax-table)
+  (modify-syntax-entry ?> "w" lispyscript-mode-syntax-table)
+  (modify-syntax-entry ?? "w" lispyscript-mode-syntax-table)
+  (setq font-lock-defaults '((lispyscript-font-lock-defaults))))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.ls$" . lispyscript-mode))
+
+(provide 'lispyscript-mode)
+;;; lispyscript-mode.el ends here
